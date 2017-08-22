61817f39f38e29d4ab63af6f90d4c9ef06b55461
dickrichards69 <31195553+dickrichards69@users.noreply.github.com>
2017-08-21 21:06:22 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -21,6 +21,9 @@
 import os
 import tweepy
 from secrets import *
+auth = tweepy.OAuthHandler(C_KEY, C_SECRET)  
+auth.set_access_token(A_TOKEN, A_TOKEN_SECRET)  
+api = tweepy.API(auth)
 from time import gmtime, strftime
 
 
