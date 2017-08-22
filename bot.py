6d84bdaad26f2e6785ab8a2862b1822be15f36be
dickrichards69 <31195553+dickrichards69@users.noreply.github.com>
2017-08-21 21:19:31 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,70 +1,189 @@
-# Copyright (c) 2015–2016 Molly White
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in
-# all copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
+# -*- coding: utf-8 -*-
 
-import os
+'''
+Copyright 2015 Randal S. Olson
+This file is part of the reddit Twitter Bot library.
+The reddit Twitter Bot library is free software: you can redistribute it and/or
+modify it under the terms of the GNU General Public License as published by the
+Free Software Foundation, either version 3 of the License, or (at your option)
+any later version.
+The reddit Twitter Bot library is distributed in the hope that it will be
+useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public
+License for more details. You should have received a copy of the GNU General
+Public License along with the reddit Twitter Bot library.
+If not, see http://www.gnu.org/licenses/.
+'''
+
+import praw
+import json
+import requests
 import tweepy
-from secrets import *
-auth = tweepy.OAuthHandler(C_KEY, C_SECRET)  
-auth.set_access_token(A_TOKEN, A_TOKEN_SECRET)  
-api = tweepy.API(auth)
-from time import gmtime, strftime
+import time
+import os
+import urllib.parse
+from glob import glob
+
+# Place your Twitter API keys here
+ACCESS_TOKEN = '899438921108639744-nGqe5sDgbaguTIXE2TghqLBPxIcpt5g'
+ACCESS_TOKEN_SECRET = 'rKWuF2NbaW61ePBY7uS14qdSJMvgQ8tPPEswbZcwbMGoz'
+CONSUMER_KEY = 'fYJDdrwEwBdfFNWd8U8elk6OE'
+CONSUMER_SECRET = '8bRnA0lew9z0vXjLfowdKuNtm1d2V8yil7WYrQTIeygGeHXvrU'
+
+# Place the subreddit you want to look up posts from here
+SUBREDDIT_TO_MONITOR = 'https://www.reddit.com/r/aww/'
+
+# Place the name of the folder where the images are downloaded
+IMAGE_DIR = 'img'
+
+# Place the name of the file to store the IDs of posts that have been posted
+POSTED_CACHE = 'posted_posts.txt'
+
+# Place the string you want to add at the end of your tweets (can be empty)
+TWEET_SUFFIX = '#Aww #Animals'
+
+# Place the maximum length for a tweet
+TWEET_MAX_LEN = 140
+
+# Place the time you want to wait between each tweets (in seconds)
+DELAY_BETWEEN_TWEETS = 10800
+
+# Place the lengths of t.co links (cf https://dev.twitter.com/overview/t.co)
+T_CO_LINKS_LEN = 24
+
+def setup_connection_reddit(subreddit):
+    ''' Creates a connection to the reddit API. '''
+    print('[bot] Setting up connection with reddit')
+    reddit_api = praw.Reddit('reddit Twitter tool monitoring {}'.format(subreddit))
+    return reddit_api.get_subreddit(subreddit)
 
 
-# ====== Individual bot configuration ==========================
-bot_username = ''
-logfile_name = bot_username + ".log"
+def tweet_creator(subreddit_info):
+    ''' Looks up posts from reddit and shortens the URLs to them. '''
+    post_dict = {}
+    post_ids = []
 
-# ==============================================================
+    print('[bot] Getting posts from reddit')
+
+    # You can use the following "get" functions to get posts from reddit:
+    #   - get_top(): gets the most-upvoted posts (ignoring post age)
+    #   - get_hot(): gets the most-upvoted posts (taking post age into account)
+    #   - get_new(): gets the newest posts
+    #
+    # "limit" tells the API the maximum number of posts to look up
+
+    for submission in subreddit_info.get_hot(limit=5):
+        if not already_tweeted(submission.id):
+            # This stores a link to the reddit post itself
+            # If you want to link to what the post is linking to instead, use
+            # "submission.url" instead of "submission.permalink"
+            post_dict[submission.title] = {}
+            post = post_dict[submission.title]
+            post['link'] = submission.permalink
+
+            # Store the url the post points to (if any)
+            # If it's an imgur URL, it will later be downloaded and uploaded alongside the tweet
+            post['img_path'] = get_image(submission.url)
+
+            post_ids.append(submission.id)
+        else:
+            print('[bot] Already tweeted: {}'.format(str(submission)))
+
+    return post_dict, post_ids
 
 
-def create_tweet():
-    """Create the text of the tweet you want to send."""
-    # Replace this with your code!
-    text = ""
-    return text
+def already_tweeted(post_id):
+    ''' Checks if the reddit Twitter bot has already tweeted a post. '''
+    found = False
+    with open(POSTED_CACHE, 'r') as in_file:
+        for line in in_file:
+            if post_id in line:
+                found = True
+                break
+    return found
 
 
-def tweet(text):
-    """Send out the text as a tweet."""
-    # Twitter authentication
-    auth = tweepy.OAuthHandler(C_KEY, C_SECRET)
-    auth.set_access_token(A_TOKEN, A_TOKEN_SECRET)
+def strip_title(title, num_characters):
+    ''' Shortens the title of the post to the 140 character limit. '''
+
+    # How much you strip from the title depends on how much extra text
+    # (URLs, hashtags, etc.) that you add to the tweet
+    # Note: it is annoying but some short urls like "data.gov" will be
+    # replaced by longer URLs by twitter. Long term solution could be to
+    # use urllib.parse to detect those.
+    if len(title) <= num_characters:
+        return title
+    else:
+        return title[:num_characters - 1] + '…'
+
+
+def get_image(img_url):
+    ''' Downloads i.imgur.com images that reddit posts may point to. '''
+    if 'imgur.com' in img_url:
+        file_name = os.path.basename(urllib.parse.urlsplit(img_url).path)
+        img_path = IMAGE_DIR + '/' + file_name
+        print('[bot] Downloading image at URL ' + img_url + ' to ' + img_path)
+        resp = requests.get(img_url, stream=True)
+        if resp.status_code == 200:
+            with open(img_path, 'wb') as image_file:
+                for chunk in resp:
+                    image_file.write(chunk)
+            # Return the path of the image, which is always the same since we just overwrite images
+            return img_path
+        else:
+            print('[bot] Image failed to download. Status code: ' + resp.status_code)
+    else:
+        print('[bot] Post doesn\'t point to an i.imgur.com link')
+    return ''
+
+
+def tweeter(post_dict, post_ids):
+    ''' Tweets all of the selected reddit posts. '''
+    auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+    auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
     api = tweepy.API(auth)
 
-    # Send the tweet and log success or failure
-    try:
-        api.update_status(text)
-    except tweepy.error.TweepError as e:
-        log(e.message)
-    else:
-        log("Tweeted: " + text)
+    for post, post_id in zip(post_dict, post_ids):
+        img_path = post_dict[post]['img_path']
+
+        extra_text = ' ' + post_dict[post]['link'] + TWEET_SUFFIX
+        extra_text_len = 1 + T_CO_LINKS_LEN + len(TWEET_SUFFIX)
+        if img_path:  # Image counts as a link
+            extra_text_len += T_CO_LINKS_LEN
+        post_text = strip_title(post, TWEET_MAX_LEN - extra_text_len) + extra_text
+        print('[bot] Posting this link on Twitter')
+        print(post_text)
+        if img_path:
+            print('[bot] With image ' + img_path)
+            api.update_with_media(filename=img_path, status=post_text)
+        else:
+            api.update_status(status=post_text)
+        log_tweet(post_id)
+        time.sleep(DELAY_BETWEEN_TWEETS)
 
 
-def log(message):
-    """Log message to logfile."""
-    path = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
-    with open(os.path.join(path, logfile_name), 'a+') as f:
-        t = strftime("%d %b %Y %H:%M:%S", gmtime())
-        f.write("\n" + t + " " + message)
+def log_tweet(post_id):
+    ''' Takes note of when the reddit Twitter bot tweeted a post. '''
+    with open(POSTED_CACHE, 'a') as out_file:
+        out_file.write(str(post_id) + '\n')
 
 
-if __name__ == "__main__":
-    tweet_text = create_tweet()
-    tweet(tweet_text)
+def main():
+    ''' Runs through the bot posting routine once. '''
+    # If the tweet tracking file does not already exist, create it
+    if not os.path.exists(POSTED_CACHE):
+        with open(POSTED_CACHE, 'w'):
+            pass
+    if not os.path.exists(IMAGE_DIR):
+        os.makedirs(IMAGE_DIR)
+
+    subreddit = setup_connection_reddit(SUBREDDIT_TO_MONITOR)
+    post_dict, post_ids = tweet_creator(subreddit)
+    tweeter(post_dict, post_ids)
+
+    # Clean out the image cache
+    for filename in glob(IMAGE_DIR + '/*'):
+        os.remove(filename)
+
+if __name__ == '__main__':
+    main()
